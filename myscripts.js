25f86bcf3ceab9b0753aefd64051c305c1da7a4b
yxteo <106210307+yxteo@users.noreply.github.com>
2023-09-20 11:51:03 +0800

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,51 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>STTSS 2024 J1 Book Order Form</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-    <form action="https://docs.google.com/forms/d/e/1FAIpQLSca4qVHLURdQl_YDB8KnB_TUhNSqr8HwyZvcrbdhvigdKwXzA/formResponse" target="_self" id="bootstrapForm" method="POST">
-        <fieldset>
-            <h2>Testing book order<br><small></small></h2>
-        </fieldset>
-        <!-- Field type: "checkboxes" id: "589352420" -->
-        <fieldset>
-            <legend for="589352420">Book</legend>
-            <div class="form-group">
-                <div class="checkbox" id="ch">
-                    <label>
-                        <input type="checkbox" name="entry.2003876057" value='1' onclick="updateTotal()">
-                        1
-                    </label>    
-                    <label>
-                        <input type="checkbox" name="entry.2003876057" value='2' onclick="updateTotal()">
-                        2
-                    </label>
-                </div>
-            </div>
-        </fieldset>
-        <input type="hidden" name="fvv" value="1">
-        <input type="hidden" name="fbzx" value="-2739287917357523008">
-        <div id="totalValue">Total Value: 0</div>    
-        <input type="hidden" name="pageHistory" value="0">
-        <input class="btn btn-primary" type="submit" value="Submit">
-    </form>
-    <script src="myscripts.js"></script>
-    <script>
-        function updateTotal() {
-            var checkboxes = document.querySelectorAll('input[type="checkbox"][name="entry.2003876057"]');
-            var total = 0;
-            checkboxes.forEach(function(checkbox) {
-                if (checkbox.checked) {
-                    total += parseInt(checkbox.value);
-                }
-            });
-            document.getElementById('totalValue').textContent = 'Total Value: ' + total;
+@@ -1,7 +1,8 @@
+// This script requires jQuery and jquery-form plugin
+// You can use these ones from Cloudflare CDN:
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js" integrity="sha256-2Pjr1OlpZMY6qesJM68t2v39t+lMLvxwpa8QlRjJroA=" crossorigin="anonymous"></script>
+<>// This script requires jQuery and jquery-form plugin
+    // You can use these ones from Cloudflare CDN:
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js" integrity="sha256-2Pjr1OlpZMY6qesJM68t2v39t+lMLvxwpa8QlRjJroA=" crossorigin="anonymous"></script></>
+//
+$('#bootstrapForm').submit(function (event) {
+    event.preventDefault()
+@@ -14,7 +15,22 @@ $('#bootstrapForm').submit(function (event) {
+            // does not support it, so this is handled as a failure.
+            alert('Form Submitted. Thanks.')
+            // You can also redirect the user to a custom thank-you page:
+            window.location = 'thankyoupage.html';
+            window.location = 'http://www.mydomain.com/thankyoupage.html';
         }
-    </script>
-</body>
-</html>
+    })
+})
